1749a81d4aa646c6a514371d9cf2cf5d5c9a1d7b
ewright516 <65744465+ewright516@users.noreply.github.com>
2023-10-24 11:43:22 -0400

    working

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for authserver project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,40 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-
-import oauth2_provider.views as oauth2_views
-from ..authserver.views import ApiEndpoint
-
-oauth2_endpoint_views = [
-    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path('token/', oauth2_views.TokenView.as_view(), name="token"),
-    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
-]
-
-if settings.DEBUG:
-    # OAuth2 Application Management endpoints
-    oauth2_endpoint_views += [
-        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
-        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
-        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
-        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
-        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
-    ]
-
-    # OAuth2 Token Management endpoints
-    oauth2_endpoint_views += [
-        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
-            name="authorized-token-delete"),
-    ]
+from django.contrib.auth.views import LoginView
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    # OAuth 2 endpoints:
-    # need to pass in a tuple of the endpoints as well as the app's name
-    # because the app_name attribute is not set in the included module
     path('admin/', admin.site.urls),
-    path('oauth2/', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
-    path('api/hello', ApiEndpoint.as_view()),  # an example resource endpoint
+    path('registration/', include("django.contrib.auth.urls")),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
 ]
