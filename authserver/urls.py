9e4d6df4ed6c207a468499c076bc6d47bb290228
ewright516 <65744465+ewright516@users.noreply.github.com>
2023-12-04 11:01:15 -0500

    sign up api route workign

--- authserver/urls.py
+++ authserver/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import signup, verify
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path("verify/<int:code>", verify, name='verify')
+    path('signup', signup, name='signup'),
+    path('verify/<int:code>/', verify, name='verify')
 ]
--- authserver/views.py
+++ authserver/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from oauth2_provider.views.generic import ProtectedResourceView
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import login, authenticate, logout
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
 import json
 import uuid
 from .models import User
@@ -11,22 +13,39 @@ from .forms import SignUpForm
 # Create your views here.
 
 
+@csrf_exempt
 def signup(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
+        value = request.POST['email'].split("@")
+        username = value[0]
+        body = {
+            'email': request.POST['email'],
+            'username': username,
+            'password1': request.POST['password'],
+            'password2': request.POST['password']
+        }
+        form = SignUpForm(body)
         if form.is_valid():
             user = form.save()
             user.email_code = uuid.uuid4()
             user.save()
             response_data = {
-                'user': user,
-                'email-code': user.email_code
+                'email-code': f"http://127.0.0.1:8000/users/verify/{user.email_code}"
             }
             return JsonResponse(response_data)
+        else:
+            return JsonResponse({'errors': form.errors})
+    else:
+        raise PermissionDenied
 
 
+@csrf_exempt
 def verify(request, code):
     user = User.objects.get(email_code=code)
+    user.verified = True
+    user.save()
+    return JsonResponse({'verified': True})
+
 
 
 
